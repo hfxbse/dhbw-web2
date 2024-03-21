983589acd4689b4d55a8e280e2732231d0f5c1b6
Fabian Haas <inf22008@lehre.dhbw-stuttgart.de>
2024-03-21 18:39:23 +0100

    add basic login

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello world")
+import {createInterface} from "readline";
+import {stdin, stdout} from "process";
+import {login} from "./instagram.js";
+
+const prompt = createInterface({
+    input: stdin,
+    output: stdout
+})
+
+function ask(question: string): Promise<string> {
+    return new Promise((resolve) => prompt.question(question, resolve))
+}
+
+const user = await ask("Instagram username, phone number, or email: ")
+const password = await ask("Password: ")
+
+prompt.close()
+
+console.log(await login({user, password}));
---
+++ src/instagram.ts
@@ -0,0 +1,105 @@
+import hexToArrayBuffer from "hex-to-array-buffer";
+import sealBox from "tweetnacl-sealedbox-js"
+
+const crypto = globalThis.crypto
+const encoder = new TextEncoder()
+
+async function fetchVerification(): Promise<{ csrf: string, keyVersion: number, keyId: number, publicKey: string }> {
+    const response = await fetch("https://www.instagram.com/api/v1/web/data/shared_data/", {
+        headers: {
+            "Sec-Fetch-Site": "same-origin"
+        }
+    })
+
+    const data = await response.json() as {
+        config: {
+            csrf_token: string
+        },
+        encryption: {
+            key_id: string,
+            public_key: string,
+            version: string
+        }
+    }
+
+    return {
+        csrf: data.config.csrf_token,
+        keyId: parseInt(data.encryption.key_id, 10),
+        publicKey: data.encryption.public_key,
+        keyVersion: parseInt(data.encryption.version, 10),
+    }
+}
+
+async function encryptPassword({password, keyId, publicKey}: {
+    password: string,
+    keyId: number,
+    publicKey: string,
+}) {
+    const passwordBuffer = encoder.encode(password)
+    const time = (Date.now() / 1000).toFixed(0);
+
+    if (publicKey.length !== 64) throw new Error("Wrong public key hex.")
+    const keyBuffer = new Uint8Array(hexToArrayBuffer(publicKey))
+
+    const target = new Uint8Array(100 + passwordBuffer.length)
+    target.set([1, keyId])
+
+    const algorithmName = "AES-GCM"
+    const rawKeys = await crypto.subtle.generateKey({
+        length: keyBuffer.byteLength * 8,
+        name: algorithmName
+    }, true, ['encrypt', 'decrypt'])
+
+    const iv = new Uint8Array(12)
+
+    const exportedKeys = await crypto.subtle.exportKey("raw", rawKeys)
+    const cipher = new Uint8Array(await crypto.subtle.encrypt({
+        additionalData: encoder.encode(time),
+        iv,
+        name: algorithmName,
+        tagLength: 16 * 8
+    }, rawKeys, passwordBuffer.buffer))
+
+    const box = sealBox.seal(new Uint8Array(exportedKeys), keyBuffer)
+    if (box.length !== 48 + 32) throw new Error('Encrypted key is the wrong length');
+
+    target.set([box.length, box.length >> 8 & 255], 2)
+    target.set(box, 4)
+
+    target.set(cipher.slice(-16), 84)
+    target.set(cipher.slice(0, -16), 100)
+
+    const converted = []
+    target.forEach(element => converted.push(String.fromCharCode(element)))
+
+    return {time, encryptedPassword: btoa(converted.join(''))}
+}
+
+export async function login({user, password}: { user: string, password: string }): Promise<string> {
+    const verification = await fetchVerification()
+    const {time, encryptedPassword} = await encryptPassword({...verification, password})
+
+    const data = new FormData()
+    data.set("username", user)
+    data.set(
+        "enc_password",
+        `#PWD_INSTAGRAM_BROWSER:${verification.keyVersion}:${time}:${encryptedPassword}`
+    )
+
+    const response = await fetch("https://www.instagram.com/api/v1/web/accounts/login/ajax/", {
+        method: "POST",
+        body: data,
+        headers: {
+            "X-CSRFToken": verification.csrf,
+            "Sec-Fetch-Site": "same-origin"
+        }
+    })
+
+    const identifier = "sessionid="
+    const identify = (cookie) => cookie.startsWith(identifier)
+
+    return response.headers
+        .getSetCookie().find(identify)
+        .split(";").find(identify)
+        .substring(identifier.length)
+}
