231e4519ff7590d429743bef66efd41d65284130
Fabian Haas <inf22008@lehre.dhbw-stuttgart.de>
2024-03-22 11:07:12 +0100

    mask password input

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,12 @@
-import {createInterface} from "readline";
-import {stdin, stdout} from "process";
+import * as prompt from '@inquirer/prompts';
 import {login} from "./instagram.js";
 
 
 async function authenticate() {
-    const prompt = createInterface({
-        input: stdin,
-        output: stdout
-    })
-
-    function ask(question: string): Promise<string> {
-        return new Promise((resolve) => prompt.question(question, resolve))
-    }
-
     while (true) {
         try {
-            const user = await ask("Instagram username, phone number, or email: ")
-            const password = await ask("Password: ")
+            const user = await prompt.input({message: "Instagram username, phone number, or email: "})
+            const password = await prompt.password({message: "Password: "})
 
             return await login({user, password})
         } catch (e) {
