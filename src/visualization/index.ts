7aaf919119644b544dad5d1aef08e02a43d7c8ad
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-05-02 17:00:40 +0200

    mark placeholders

--- src/visualization/index.ts
+++ src/visualization/index.ts
@@ -1,27 +1,29 @@
 import UserGraphVisualization from "./graph/graph";
 import GraphToolbar from "./toolbar/toolbar";
-import example from './example'
 import './layout.css'
-import {User} from "../instagram/user";
+import {User, UserGraph} from "../instagram/user";
 
 customElements.define('user-graph', UserGraphVisualization)
 customElements.define('graph-toolbar', GraphToolbar)
 
 
 window.addEventListener("DOMContentLoaded", () => {
+    const data = "REPLACE-ME-WITH-USER-GRAPH"
+    const graph: UserGraph = JSON.parse(data)
+
     const visualization = document.querySelector("user-graph") as UserGraphVisualization;
-    visualization.setAttribute(UserGraphVisualization.graphAttribute, JSON.stringify(example))
+    visualization.setAttribute(UserGraphVisualization.graphAttribute, data)
 
     const toolbar = document.querySelector("graph-toolbar") as GraphToolbar;
     toolbar.addEventListener("remove-highlighting", () => visualization.removeHighlights());
     toolbar.addEventListener("reset-positioning", () => visualization.resetPositioning())
 
     toolbar.addEventListener("search-user", function (event: CustomEvent) {
-        const matchingUser = Object.values(example).find((user: User) => user.profile.username === event.detail);
+        const matchingUser = Object.values(graph).find((user: User) => user.profile.username === event.detail);
 
         if (!matchingUser) return toolbar.setSearchError(`No user found with the exact username: ${event.detail}`)
 
-        visualization.highlightUserLinks(matchingUser);
+        visualization.highlightUserLinks(matchingUser as User);
         toolbar.clearSearch();
     });
 })
